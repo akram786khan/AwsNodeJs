e96b49d6f3df80c80e204d0cc365119f165c4229
a <a@as-MacBook-Air.local>
2023-03-26 17:50:31 +0530

    update8

--- backend/routes/userAuthRoutes.js
+++ backend/routes/userAuthRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getMe } = require('../controllers/userAuthController')
+//const { registerUser, loginUser, getMe } = require('../controllers/userAuthController')
 //const { protect } = require('../middleware/authMiddleware');
-router.post('/signup', registerUser);
-router.post('/login', loginUser);
+// router.post('/signup', registerUser);
+// router.post('/login', loginUser);
 //router.get('/me', protect, getMe);
-// router.post('/signup', (req, res) => {
-//     res.send("this is signUp route")
-// });
-// router.post('/login', (req, res) => {
-//     res.send("this is login route")
-// });
+router.post('/signup', (req, res) => {
+    res.send("this is signUp route")
+});
+router.post('/login', (req, res) => {
+    res.send("this is login route")
+});
 // router.get('/me', (req, res) => {
 //     res.send("this is get me route")
 // });
