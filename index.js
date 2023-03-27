cfef61e17a35bff0e20c64114a734cff200b6195
a <a@as-MacBook-Air.local>
2023-03-27 17:09:58 +0530

    update17

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const mongoose = require("mongoose");
 const cors = require('cors')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,7 +8,11 @@ const port = process.env.PORT || 5000;
 //const ConnectDB = require('./backend/config/db');
 const app = express();
 //ConnectDB();
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.ACCESS,
+    })
+);
 app.use(express.json());
 app.get("/myy", (req, res) => {
     res.send("Hello....")
@@ -8458,3 +8463,15 @@ app.listen(port, () => {
 // app.listen(port, () => {
 //     console.log(`app server started on port ${port}`)
 // })
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+    })
+    .then(async (data) => {
+        console.log("MongoDB Connected.!");
+        const server = app.listen(port);
+    })
+    .catch((err) => console.log(err));
